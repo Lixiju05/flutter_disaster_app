01fabb0c3b74ff34376c43ec828dd5283e1d6932
你的名字 <s112213033@ncnu.edu.tw>
2026-03-13 22:50:23 +0800

    Fix Citizen model and ViewModel: rename isRescued to needsRescue, fix getters, loading, and repository

--- test/admin_APP/viewModels/citizen_viewModel.dart
+++ test/admin_APP/viewModels/citizen_viewModel.dart
@@ -1,3 +1,40 @@
 import 'package:flutter/foundation.dart';
 import '../../core/models/citizen.dart';
-import '../../core/repository/fake_repository.dart';
+import '../../core/repositories/admin_repository.dart';
+
+class CitizenViewmodel extends ChangeNotifier{
+  List<Citizen> _citizens=[];
+  List<Citizen> get Citizen => _citizens;
+
+  bool _isLodding=false;  //是否載入資料 UI根據狀態顯示loading spinner
+  bool get isLodding => _isLodding; 
+
+  CitizenViewmodel(){
+    loadCitizens();
+  }
+  //載入民眾資料
+  Future<void> loadCitizens() async{
+    _isLoading=true;
+    ChangeNotifier();
+
+    _citizens=AdminRepository.getCitizens();
+
+    _isLoading=false;
+    ChangeNotifier();
+  }
+  //搜尋民眾
+  void search(String keyword){
+    _citizens=AdminRepository.getCitizens();
+    .where((c) => c.name.toLowerCase().contains(keyword.toLowerCase())).toList();
+    ChangeNotifier();
+  }
+  //更新民眾是否需要救援
+  void updateNeedsRescue(Citizen citizen, bool needsRescue){
+    final index= _citizens.indexWhere((c) => c.id == citizen.id);
+    if (index != -1){
+      _citizens[index].needsRescue=needsRescue;
+      ChangeNotifier();
+    }
+  }
+
+}
